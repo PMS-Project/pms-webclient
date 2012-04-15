280ce72e799ad6d4714fa21e88427db37ff72f5c
Lukas Michalski <lukas.michalski@linuxarea.info>
2012-04-15 19:35:32 +0200

    Upload missing changed files

--- Application.js
+++ Application.js
@@ -20,48 +20,46 @@ extend : qx.application.Standalone,
 
 construct : function()
 {
-this.base(arguments);
-_parent = this;
+  this.base(arguments);
+  _parent = this;
+  _activeTab = "default";
 },
 
-/*
-*****************************************************************************
+/******************************************************************************
 MEMBERS
-*****************************************************************************
-*/
-
+******************************************************************************/
 members :
 {
-  _getRoot : "",
-  _tabs : new Object(),
-  _tabView : "",
-  _activeTab : "Tab1",
-  _parent : "",
+  '_tabs'       : new Object(),
+  '_widgets'    : new Object(),
+  '_parent'     : "",
+  '_activeTab'  : "",
+  'buffer'      : "",
 
-  // overridden
+/******************************************************************************
+* FUNCTION: main
+******************************************************************************/
   main : function()
   {
     this.base(arguments);
-    
     tabView = new qx.ui.tabview.TabView();
-    tabView.setMinHeight(600);
-    _getRoot = this.getRoot();
-    _getRoot.add(tabView, {edge: 25});
-
-    for(var x=1;x<10;x++)
-    {
-      var tabname = "Tab"+x;
-      this.createTab(tabname);
-    }
-
+    this.getRoot().add(tabView, {edge: 25});
+    this.createTab("default");    
+    this.createTab("test");
+    
+    this.WebSocket();    
+ 
+    
     tabView.addListener("changeSelection",function(e)  
     {
-//      this._activeTab = e.getData()[0].getLabel();
-//      _getRoot.debug("ActiveTab:"+e.getData()[0].getLabel());
-//      _parent.setReadTab(e.getData()[0].getLabel());
+      _activeTab = e.getData()[0].getLabel();
+      _parent.setTabRead(e.getData()[0].getLabel());
     });
   },
-
+  
+/******************************************************************************
+* FUNCTION: closeTab
+******************************************************************************/
   closeTab : function (ChannelName)
   {
     if(ChannelName == "default")
@@ -75,27 +73,126 @@ members :
     }
   },
 
+/******************************************************************************
+* FUNCTION: createTab
+******************************************************************************/
   createTab : function (ChannelName)
   {
-    var border = new qx.ui.decoration.Single(3, "solid", "black");
     this._tabs[ChannelName] = new qx.ui.tabview.Page(ChannelName);
     this._tabs[ChannelName].setLayout(new qx.ui.layout.Canvas());
 
-    var widget = new pms.ChatWidget();
+    this._widgets[ChannelName] = new pms.ChatWidget();
     
-    this._tabs[ChannelName].add(widget,{edge:0});
+    this._tabs[ChannelName].add(this._widgets[ChannelName],{edge:0});
     tabView.add(this._tabs[ChannelName]);
-    _parent.setUnreadTab(ChannelName);
+    this.setTabUnread(ChannelName);
   },
 
-  setUnreadTab : function (ChannelName)
+/******************************************************************************
+* FUNKTION: setTabUnread
+******************************************************************************/
+  setTabUnread : function (ChannelName)
   {
     this._tabs[ChannelName].setIcon("icon/16/apps/utilities-help.png");
   },
 
-  setReadTab : function (ChannelName)
+/******************************************************************************
+* FUNKTION: setTabRead
+******************************************************************************/
+  setTabRead : function (ChannelName)
   {
     this._tabs[ChannelName].resetIcon();
+  },
+
+/******************************************************************************
+* FUNCTION: WebSocket
+******************************************************************************/
+  WebSocket: function()
+  {
+    var buffer = "";
+    if ("WebSocket" in window)
+    {   
+      ws = new WebSocket("ws://pms.muhla-solutions.de:8888");
+      ws.onopen = function()
+      {   
+        ws.send(_parent.toNetstring("/join test"));
+      };  
+      ws.onmessage = function (evt) 
+      {        
+       buffer += evt.data;
+        
+       var str;
+       while((str = _parent.readNetstring(buffer)) != undefined){
+       var command = pms.Parser.parseMessage(str);
+        
+       if(pms.Parser.error)
+       {
+         alert(pms.Parser.error);
+       }
+       else
+       {
+          if(command.arguments[0] != _activeTab)
+          {
+          _parent.setTabUnread(command.arguments[0]);
+          }
+        _parent._widgets[command.arguments[0]].setMessage(command.arguments[1]+": "+command.arguments[3]+"=>"+_activeTab);
+       }
+       buffer = "";
+         
+      }
+       
+      };  
+      ws.onclose = function(evt)
+      {   
+        alert("close");
+      };  
+    }
+  },
+  
+/******************************************************************************
+* FUNCTION: readNetstring
+******************************************************************************/
+  readNetstring : function (buffer) 
+  {
+    var buflen = buffer.length;
+    var delim = -1;
+    for(var i = 0; i < buflen; i++){
+      var cc = buffer.charCodeAt(i);
+      if(cc == 0x3a){
+        if(i == 0){
+          return handleInvalidMessage();
+        }
+        delim = i;
+        break;
+      }
+      if (cc < 0x30 || cc > 0x39){ 
+        return handleInvalidMessage();
+      }
+    }
+    if(delim > 0){
+      var msgLenTxt = buffer.substr(0,delim);
+      var msgLen = parseInt(msgLenTxt);
+      if(msgLen === NaN){
+        return handleInvalidMessage();
+      }
+      //if the buffer is shorter than the data we need stop
+      if(msgLen+msgLenTxt.length+1+1 > buffer.length)
+        return;
+      if(buffer.charCodeAt(msgLenTxt.length+1+msgLen) != 0x2c){
+        return handleInvalidMessage();
+      }
+      var message = buffer.substr(delim+1,msgLen);
+      buffer = buffer.substr(msgLenTxt.length+1+msgLen+1);
+      return message;
+    }
+    return undefined;
+  },
+/******************************************************************************
+* FUNCTION: toNetstring
+******************************************************************************/  
+  toNetstring: function(str)
+  {
+    return str.length+":"+str+",";
   }
 }
 });
--- ChatWidget.js
+++ ChatWidget.js
@@ -6,11 +6,6 @@ Authors: Lukas Michalski, Benjamin Zeller, Thorsten Schwalb
 
 ************************************************************************ */
 
-/* ************************************************************************
-#asset(pms/*)
-#asset(qx/icon/${qx.icontheme}/16/apps/utilities-help.png)
-************************************************************************ */
-
 /**
 * This is the main application class of your custom application "pms"
 */
@@ -21,7 +16,8 @@ extend : qx.ui.container.Composite,
 construct : function()
 {
   this.base(arguments);
-  this.initialize();
+  _parentb = this;
+  this.main();
 },
 
 /*
@@ -32,35 +28,102 @@ MEMBERS
 
 members :
 {
-  m_Input : "",
-  m_Message : "",
-  m_Topic : "",
-  
-  // overridden
-  main : function()
-  {
-    this.base(arguments);
-    this.add(m_Topic);
-    this.set({backgroundColor: "green", padding: 0});
-  },
-  
-  initialize : function ()
-  {
-  m_Topic 	= 	new qx.ui.form.TextField("Topic");
-  m_Message 	= 	new qx.ui.form.TextArea("TextArea");
-  m_Input 	= 	new qx.ui.form.TextField("TextField");
-  
-  this.setLayout(new qx.ui.layout.VBox(3));
-  this.set({padding: 0});
 
-  this.add(m_Topic.set({
-    maxHeight:30
-    }), { flex : 1 });
-  this.add(m_Message.set({
-  }), { flex : 2 });
-  this.add(m_Input.set({
-    maxHeight:30
-  }), { flex : 3 });
+  _parentb : "",
+  m_Input : null,
+  m_Message : null,
+  m_Topic : null,
+  m_List : null,
+  m_Button : null,
+  m_ListData : null,
+
+/******************************************************************************
+* FUNCTION: main
+******************************************************************************/
+  main : function ()
+  { 
+    m_ListData  =  new qx.data.Array();
+    m_Topic     =  new qx.ui.form.TextField("Topic");
+    m_Message   =  new qx.ui.form.TextArea("TextArea");
+    m_Input     =  new qx.ui.form.TextField("TextField");
+    m_Button    =  new qx.ui.form.Button("Push Data");
+    m_List      =  new qx.ui.list.List(m_ListData);
+    m_UserList  =  new qx.ui.list.List(m_ListData);
+    
+    m_Input.addListener("keypress",function(e){
+      if(e.getKeyIdentifier() == "Enter")
+      {
+        this.setValue("");        
+      }
+    });
+
+    this.set( { padding: 0 } );
+    this.setLayout(new qx.ui.layout.VBox(3));
+      
+    m_Topic.setEnabled(false);
+    m_List.setSelectable(false);
+
+    this.setWidgetLayout(this);
+
+  },
+
+/******************************************************************************
+* FUNCTION: setWidgetLayout
+******************************************************************************/  
+  setWidgetLayout : function (tabView)
+  {
+    tabView.add(m_Topic.set({
+      maxHeight:30
+      }), { flex : 1 });
+ 
+    var splitpane = new qx.ui.splitpane.Pane("horizontal");
+    
+    m_UserList.setMaxWidth(150);
+    m_UserList.setMinWidth(150);
+    splitpane.add(m_UserList,1);
+
+    splitpane.add(m_List,2);
+    
+    tabView.add(splitpane.set({
+    }), { flex : 2 });  
+    
+    tabView.add(m_Input.set({
+      maxHeight:30
+    }), { flex : 3 });
+  },
+
+/******************************************************************************
+* FUNCTION: setWidgetLayout
+******************************************************************************/  
+  setMessage : function (value)
+  {
+    m_ListData.push(value);
   }
 }
 });
+/*
+ var timeFormat = {
+  displayFormat: 'd.m.Y - H:i:s',
+  
+  format: function(timestamp) {
+    // convert unix timestamp to javascript date object
+
+    var d = new Date(timestamp * 1000);
+
+    var output = this.displayFormat;
+    output = output.replace(/d/g, this.padZero(d.getDate()))
+            .replace(/m/g, this.padZero(d.getMonth()))
+            .replace(/Y/g, d.getFullYear())
+            .replace(/H/g, this.padZero(d.getHours()))
+            .replace(/i/g, this.padZero(d.getMinutes()))
+            .replace(/s/g, this.padZero(d.getSeconds()));
+    return output;
+},
+
+padZero: function(number) {
+  if (number < 10) {
+  return "0" + number.toString();
+  }
+  return number;
+};
+ */
